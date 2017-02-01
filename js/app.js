7c2f6a020c9199ae8728a83a8c6529b29289d458
gabriel melo <gabrielmeloarteaga@gmail.com>
2017-02-01 11:59:44 -0500

    get data from API and show on frontend

--- js/app.js
+++ js/app.js
@@ -2,19 +2,47 @@ var app = angular.module('imdbApp',[]);
 
 app.controller('ctrlMain', function($scope,$http){
 
-	var URL = 'http://www.omdbapi.com/?t=';
+	//Variables
+	var URL = 'http://www.omdbapi.com/?s=';
+	var results = [];
 	
+	//method search
 	$scope.getMovies = function(query){
-		query  = $scope.txtSearch;	
-
+	query  = $scope.txtSearch;
+		
+	//watcher to reset results array when start a new search
+	$scope.$watch('query', function(){
+		if(query === ''){			
+			results = [];
+		}
+	});
 	//http://stackoverflow.com/questions/41169385/http-get-success-is-not-a-function
 
-	//getting data from API
-	$http.get(URL + query).success(function(data){
-		console.log(data);
-	});
-		
+	//get data from IDMB API
+	if(query != ''){
+		//getting data from API
+		$http.get(URL + query)
+		.then( function (success){
+			//save all results	
+			results.push(success.data.Search);
+			console.log(success.data , results.length);
+		}, function( error ){
+			console.log(error);
+		 });
+		}
+
+		//filter only movies
+		movies = results.filter(function(result){
+			return result.Response === 'True';
+		});		
+
+		console.log("Movies ", results);
+
+	return $scope.movies = movies;
 	}
+
+	//method save 
+
 
 
 });
